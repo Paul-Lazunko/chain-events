48c628ff275f459fcfae215af2014b48a1ed5ee3
pashaman <plavel@gmail.com>
2021-07-28 15:08:31 +0300

    feature(lib): added custom error

---
+++ src/errors/ChainEventEmitterError.ts
@@ -0,0 +1,5 @@
+export class ChainEventEmitterError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
---
+++ src/errors/index.ts
@@ -0,0 +1,1 @@
+export { ChainEventEmitterError } from './ChainEventEmitterError';
