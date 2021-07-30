f80ffbf3f7c854732050fd6b1227dfe5abdd41c3
pashaman <plavel@gmail.com>
2021-07-30 14:22:32 +0300

    refactore(errors): refactored error messages

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -6,6 +6,6 @@ interface Errors {
 
 export const errors: Errors =  {
   invalidEventName: `Event name should be a non-empty string` ,
-  invalidEventHandler: `Provided event handler is invalid`,
+  invalidEventHandler: `Provided event handler isn't valid`,
   errorHandlerIsAbsent: (event: string) => `Error Handler wasn't set for "${event}" event`
 }
